8edbe4c37330ac383d289187af67bb1bfdcdc706
Abdeali5253 <abdeali.hazari@gmail.com>
2024-09-19 20:31:30 +0500

    made changes in backend to make it more general, and made changes in UI too for better looking

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
-// script.js
+// // script.js
+// document.getElementById('messageForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
+    
+//     const formData = new FormData();
+//     formData.append('file', document.getElementById('file').files[0]);
+//     formData.append('name_column', document.getElementById('name_column').value);
+//     formData.append('mumin_id_column', document.getElementById('mumin_id_column').value);
+//     formData.append('registered_for_column', document.getElementById('registered_for_column').value);
+//     formData.append('mobile_column', document.getElementById('mobile_column').value);
+//     formData.append('message', document.getElementById('message').value);
+
+//     fetch('http://localhost:5000/send-messages', {
+//         method: 'POST',
+//         body: formData
+//     })
+//     .then(async response => {
+//         if (!response.ok) {
+//             const err = await response.json();
+//             throw err;
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         document.getElementById('status').innerText = data.status;
+//     })
+//     .catch(error => {
+//         if (error.error) {
+//             document.getElementById('status').innerText = 'Error: ' + error.error;
+//         } else {
+//             document.getElementById('status').innerText = 'Unknown error occurred';
+//         }
+//     });
+// });
+
+
 document.getElementById('messageForm').addEventListener('submit', function(event) {
     event.preventDefault();
     
     const formData = new FormData();
     formData.append('file', document.getElementById('file').files[0]);
-    formData.append('name_column', document.getElementById('name_column').value);
-    formData.append('mumin_id_column', document.getElementById('mumin_id_column').value);
-    formData.append('registered_for_column', document.getElementById('registered_for_column').value);
     formData.append('mobile_column', document.getElementById('mobile_column').value);
+    formData.append('dynamic_columns', document.getElementById('dynamic_columns').value);  // Send the dynamic columns
     formData.append('message', document.getElementById('message').value);
 
     fetch('http://localhost:5000/send-messages', {
